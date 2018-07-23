29ed722d0069aab30d5a0cf7b8bf35b23e972235
miao1007 <miao1007@gmail.com>
2018-07-23 08:10:01 +0800

    remove q, using default Promise

--- index.js
+++ index.js
@@ -2,35 +2,33 @@ const childProcess = require('child_process');
 const fs = require('fs');
 const path = require('path');
 const crypto = require('crypto');
-const Q = require('q');
-
 const binPath = path.join(__dirname, "../.bin/mmdc");
 
 function _string2svgAsync(mmdString) {
-    const deferred = Q.defer();
     const filename = 'foo' + crypto.randomBytes(4).readUInt32LE(0) + 'bar';
     const tmpFile = "/tmp/" + filename;
-    fs.writeFile(tmpFile, mmdString, function (err) {
-        if (err) {
-            return console.log(err);
-        }
-        console.log("tmpFile =" + tmpFile);
-        childProcess.execFile(binPath, ['-i', tmpFile], function (err, stdout, stderr) {
-            if (err || stderr) {
-                console.log("err=");
-                console.log(err || stderr);
-                fs.unlinkSync(tmpFile);
-                deferred.reject(err || stderr)
-            } else {
-                console.log(stdout);
-                const text = fs.readFileSync(tmpFile + '.svg', 'utf8');
-                fs.unlinkSync(tmpFile);
-                fs.unlinkSync(tmpFile + '.svg');
-                deferred.resolve(text)
+    return new Promise((resolve, reject) => {
+        fs.writeFile(tmpFile, mmdString, function (err) {
+            if (err) {
+                return console.log(err);
             }
+            console.log("tmpFile =" + tmpFile);
+            childProcess.execFile(binPath, ['-i', tmpFile], function (err, stdout, stderr) {
+                if (err || stderr) {
+                    console.log("err=");
+                    console.log(err || stderr);
+                    fs.unlinkSync(tmpFile);
+                    reject(err || stderr)
+                } else {
+                    console.log(stdout);
+                    const text = fs.readFileSync(tmpFile + '.svg', 'utf8');
+                    fs.unlinkSync(tmpFile);
+                    fs.unlinkSync(tmpFile + '.svg');
+                    resolve(text)
+                }
+            });
         });
-    });
-    return deferred.promise;
+    })
 }
 
 module.exports = {
@@ -57,7 +55,7 @@ module.exports = {
                 var rawBlock = match[0];
                 var mermaidContent = match[1];
                 console.log(mermaidContent)
-                var processed = _string2svgAsync(mermaidContent);
+                var processed =  _string2svgAsync(mermaidContent);
                 await processed;
                 page.content = page.content.replace(rawBlock, processed);
             }
